77cc3a4b2cc75911a91c5a4e18e9f223a639212c
juvervae <jules.vervaeke@ugent.be>
2020-10-25 11:14:47 +0100

    style is now added from the Controlla class, used code that I generated with python, so will have to clean this up later on

--- code_generator/ClickablePane.java
+++ code_generator/ClickablePane.java
@@ -1,5 +1,7 @@
 package code_generator;
 
+import javafx.collections.ObservableList;
+import javafx.css.Style;
 import javafx.scene.layout.Pane;
 
 public class ClickablePane extends Pane implements Listener {
@@ -7,10 +9,18 @@ public class ClickablePane extends Pane implements Listener {
     private int row;
     private int column;
 
+    public ClickablePane(){
+        super();
+    }
+
     public ClickablePane(int x, int y){
         super();
         this.row = x;
         this.column = y;
+    }
+
+    public ObservableList<String> getStyleClassAsObservableList(){
+        return super.getStyleClass();
     }
 
     @Override
@@ -19,6 +29,13 @@ public class ClickablePane extends Pane implements Listener {
             this.getStylesheets().add("code_generator.css");
             this.getStyleClass().add("");
         }
+    }
 
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
     }
 }
--- code_generator/CodeGeneratorControlla.java
+++ code_generator/CodeGeneratorControlla.java
@@ -1,33 +1,74 @@
 package code_generator;
 
-import javafx.scene.layout.Pane;
-
 public class CodeGeneratorControlla {
-    public Pane pane_0_0;
-    public Pane pane_0_1;
-    public Pane pane_0_2;
-    public Pane pane_0_3;
-    public Pane pane_0_4;
-    public Pane pane_1_0;
-    public Pane pane_1_1;
-    public Pane pane_1_2;
-    public Pane pane_1_3;
-    public Pane pane_1_4;
-    public Pane pane_2_0;
-    public Pane pane_2_1;
-    public Pane pane_2_2;
-    public Pane pane_2_3;
-    public Pane pane_2_4;
-    public Pane pane_3_0;
-    public Pane pane_3_1;
-    public Pane pane_3_2;
-    public Pane pane_3_3;
-    public Pane pane_3_4;
-    public Pane pane_4_0;
-    public Pane pane_4_1;
-    public Pane pane_4_2;
-    public Pane pane_4_3;
-    public Pane pane_4_4;
+    public ClickablePane pane_0_0;
+    public ClickablePane pane_0_1;
+    public ClickablePane pane_0_2;
+    public ClickablePane pane_0_3;
+    public ClickablePane pane_0_4;
+    public ClickablePane pane_1_0;
+    public ClickablePane pane_1_1;
+    public ClickablePane pane_1_2;
+    public ClickablePane pane_1_3;
+    public ClickablePane pane_1_4;
+    public ClickablePane pane_2_0;
+    public ClickablePane pane_2_1;
+    public ClickablePane pane_2_2;
+    public ClickablePane pane_2_3;
+    public ClickablePane pane_2_4;
+    public ClickablePane pane_3_0;
+    public ClickablePane pane_3_1;
+    public ClickablePane pane_3_2;
+    public ClickablePane pane_3_3;
+    public ClickablePane pane_3_4;
+    public ClickablePane pane_4_0;
+    public ClickablePane pane_4_1;
+    public ClickablePane pane_4_2;
+    public ClickablePane pane_4_3;
+    public ClickablePane pane_4_4;
+
+    private CodeGeneratorModel model;
+
+    public void initialize(){
+        System.out.println("test");
+        this.model = new CodeGeneratorModel();
+        pane_0_0.setRow(0);
+        pane_1_0.setRow(1);
+        pane_2_0.setRow(2);
+        pane_3_0.setRow(3);
+        pane_4_0.setRow(4);
+        pane_0_0.setColumn(0);
+        pane_1_0.setColumn(1);
+        pane_2_0.setColumn(2);
+        pane_3_0.setColumn(3);
+        pane_4_0.setColumn(4);
+        pane_0_0.getStyleClassAsObservableList().add("button-like");
+        pane_0_1.getStyleClassAsObservableList().add("button-like");
+        pane_0_2.getStyleClassAsObservableList().add("button-like");
+        pane_0_3.getStyleClassAsObservableList().add("button-like");
+        pane_0_4.getStyleClassAsObservableList().add("button-like");
+        pane_1_0.getStyleClassAsObservableList().add("button-like");
+        pane_1_1.getStyleClassAsObservableList().add("button-like");
+        pane_1_2.getStyleClassAsObservableList().add("button-like");
+        pane_1_3.getStyleClassAsObservableList().add("button-like");
+        pane_1_4.getStyleClassAsObservableList().add("button-like");
+        pane_2_0.getStyleClassAsObservableList().add("button-like");
+        pane_2_1.getStyleClassAsObservableList().add("button-like");
+        pane_2_2.getStyleClassAsObservableList().add("button-like");
+        pane_2_3.getStyleClassAsObservableList().add("button-like");
+        pane_2_4.getStyleClassAsObservableList().add("button-like");
+        pane_3_0.getStyleClassAsObservableList().add("button-like");
+        pane_3_1.getStyleClassAsObservableList().add("button-like");
+        pane_3_2.getStyleClassAsObservableList().add("button-like");
+        pane_3_3.getStyleClassAsObservableList().add("button-like");
+        pane_3_4.getStyleClassAsObservableList().add("button-like");
+        pane_4_0.getStyleClassAsObservableList().add("button-like");
+        pane_4_1.getStyleClassAsObservableList().add("button-like");
+        pane_4_2.getStyleClassAsObservableList().add("button-like");
+        pane_4_3.getStyleClassAsObservableList().add("button-like");
+        pane_4_4.getStyleClassAsObservableList().add("button-like");
+
+    }
 
 
 }
